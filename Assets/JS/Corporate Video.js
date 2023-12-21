3c6436c25b3de2e5d0f3bf9592f3fb6387c78df0
Vamsi Kurli <n00466003@humbermail.com>
2023-12-21 16:30:30 -0500

    Initial commit

---
+++ Assets/JS/Corporate Video.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "600px";
+    x.style.width = "400px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "200px";
+  }
---
+++ Assets/JS/portfolio files.js
@@ -0,0 +1,19 @@
+var portfolioItems = [
+    {
+        image:"Previous works/Graphic Design/Pizza Corner - Desktop.png ",
+        description:"Designed a website for a new business called Pizza Corner ",
+        title:"Web page design",
+        category: "Graphic Design"
+    },
+    {
+        image:"Previous works/Film Production/Interrupted-Marriage-1.jpg",
+        description:"A 50 min films written & directed by me. And also edited by me. ",
+        title:"Interrupted Marriage",
+        category: "Film Production"
+    },
+    {
+        image:"Pictures/Chicken/Buttermilk Fried Chicken.jpg",
+        description:"Buttermilk fried chicken,crisped to perfection.",
+        title:"Buttermilk Fried Chicken",
+        category: "chicken"
+    ]
